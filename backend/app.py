fcea25b21adbc3c95e4b0440cc3067dd65a72345
Mateo_Liberatore <83372310+MateoLiberatore@users.noreply.github.com>
2025-11-17 15:41:33 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ CORS(
             "http://localhost:5173",
             "http://127.0.0.1:5173",
             "http://frontend:5173",
-            "https://mateoliberatore.github.io/PreCode"
+            "https://mateoliberatore.github.io"
         ]
     }},
     supports_credentials=True,
